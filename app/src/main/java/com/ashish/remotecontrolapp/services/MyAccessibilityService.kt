229664e45df0467cc752563a16c56077a5db424f
Ashish Pathak <pathak.ashish87@gmail.com>
2021-08-13 14:52:34 +0530

    Add MyAccessibilityService which requests controlling UI

---
+++ app/src/main/java/com/ashish/remotecontrolapp/services/MyAccessibilityService.kt
@@ -0,0 +1,48 @@
+package com.ashish.remotecontrolapp.services
+
+import android.accessibilityservice.AccessibilityService
+import android.content.Intent
+import android.view.accessibility.AccessibilityEvent
+
+class MyAccessibilityService : AccessibilityService() {
+
+    /**
+     * This system calls this method when it successfully connects to your accessibility service.
+     * Use this method to do any one-time setup steps for your service, including connecting to user
+     * feedback system services, such as the audio manager or device vibrator. If you want to set
+     * the configuration of your service at runtime or make one-time adjustments, this is a
+     * convenient location from which to call setServiceInfo().
+     */
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+    }
+
+    /**
+     * This method is called back by the system when it detects an AccessibilityEvent that matches
+     * the event filtering parameters specified by your accessibility service. For example, when the
+     * user clicks a button or focuses on a user interface control in an application for which your
+     * accessibility service is providing feedback. When this happens, the system calls this method,
+     * passing the associated AccessibilityEvent, which the service can then interpret and use to
+     * provide feedback to the user. This method may be called many times over the lifecycle of your
+     * service.
+     */
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+
+    }
+
+    /**
+     * This method is called when the system wants to interrupt the feedback your service is
+     * providing, usually in response to a user action such as moving focus to a different control.
+     * This method may be called many times over the lifecycle of your service.
+     */
+    override fun onInterrupt() {
+
+    }
+
+    /**
+     * Called by the system when the service is being shutdown for any reason
+     */
+    override fun onUnbind(intent: Intent?): Boolean {
+        return super.onUnbind(intent)
+    }
+}
